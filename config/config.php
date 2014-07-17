7858e733394f571674e5674149582f13cf54c098
Vitor Pêgas <vitorpegas95@gmail.com>
2014-07-17 23:03:06 +0100

    Update config.php

--- config/config.php
+++ config/config.php
@@ -10,25 +10,18 @@ ob_start();
 	//include Database Configuration by Alain Bertrand (a_bertrand)
 	include_once "dbClass.php";
 	//Initiate a Database Connection
-	/*
+	
 	$DB_HOST = "localhost";	//Host
 	$DB_USER = "root";		//Username
 	$DB_PASS = "";			//Password
 	$DB_NAME = "marketplace";		//DB Name
-	*/
-	
-	$DB_HOST = "localhost";	//Host
-	$DB_USER = "oryzhonc_user";		//Username
-	$DB_PASS = "e#nC}[m{UZw4";			//Password
-	$DB_NAME = "oryzhonc_marketplace";		//DB Name
-	
 	
 	$db = new Database($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);		//$db will be our database connection.
 	
 	
 	//Main Config	
 	$SITE_NAME = "Marketplace";
-	$SITE_ROOT = "http://www.oryzhon.com/marketplace/";
+	$SITE_ROOT = "http://localhost/marketplace/";
 	$MAX_SIZE = 2097152;		//2MB
 	$ALLOWED = array('jpg', 'png', 'gif');
 	$RESULTS_LIMIT = 12;
